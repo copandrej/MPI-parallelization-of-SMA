1417fcfc1a682f584447aca5e18526c8685be87c
Andrej Čop <andrej7.cop@gmail.com>
2025-01-17 11:23:54 +0100

    fix to main and arguements

--- codegen/lib/runProgram/examples/main.c
+++ codegen/lib/runProgram/examples/main.c
@@ -1,44 +1,9 @@
-/*
- * Academic License - for use in teaching, academic research, and meeting
- * course requirements at degree granting institutions only.  Not for
- * government, commercial, or other organizational use.
- *
- * main.c
- *
- * Code generation for function 'main'
- *
- */
-
-/*************************************************************************/
-/* This automatically generated example C main file shows how to call    */
-/* entry-point functions that MATLAB Coder generated. You must customize */
-/* this file for your application. Do not modify this file directly.     */
-/* Instead, make a copy of this file, modify it, and integrate it into   */
-/* your development environment.                                         */
-/*                                                                       */
-/* This file initializes entry-point function arguments to a default     */
-/* size and value before calling the entry-point functions. It does      */
-/* not store or use any values returned from the entry-point functions.  */
-/* If necessary, it does pre-allocate memory for returned values.        */
-/* You can use this file as a starting point for a main function that    */
-/* you can deploy in your application.                                   */
-/*                                                                       */
-/* After you copy the file, and before you deploy it, you must make the  */
-/* following changes:                                                    */
-/* * For variable-size function arguments, change the example sizes to   */
-/* the sizes that your application requires.                             */
-/* * Change the example values of function arguments to the values that  */
-/* your application requires.                                            */
-/* * If the entry-point functions return values, store these values or   */
-/* otherwise use them as required by your application.                   */
-/*                                                                       */
-/*************************************************************************/
-
-/* Include files */
 #include "main.h"
 #include "rt_nonfinite.h"
 #include "runProgram.h"
 #include "runProgram_terminate.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /* Function Declarations */
 static double argInit_real_T(void);
@@ -51,26 +16,24 @@ static double argInit_real_T(void)
 
 int main(int argc, char **argv)
 {
-  (void)argc;
-  (void)argv;
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <showPlot> <NrCard>\n", argv[0]);
+    return 1;
+  }
+
+  double showPlot = atof(argv[1]);
+  double NrCard = atof(argv[2]);
+
   /* The initialize function is being called automatically from your entry-point
    * function. So, a call to initialize is not included here. */
   /* Invoke the entry-point functions.
-You can call entry-point functions multiple times. */
-  main_runProgram();
+  You can call entry-point functions multiple times. */
+  runProgram(showPlot, NrCard);
+
   /* Terminate the application.
-You do not need to do this more than one time. */
+  You do not need to do this more than one time. */
   runProgram_terminate();
   return 0;
-}
-
-void main_runProgram(void)
-{
-  double showPlot_tmp;
-  /* Initialize function 'runProgram' input arguments. */
-  showPlot_tmp = argInit_real_T();
-  /* Call the entry-point 'runProgram'. */
-  runProgram(showPlot_tmp, showPlot_tmp);
 }
 
 /* End of code generation (main.c) */
